2e19be7555f073a225d481b62c258cbf9f44774e
Ddyson "peiLamed" B <fmdasani@gmail.com>
2026-08-18 19:31:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //Hello! most of this code is mine, some is from StackOverflow and other helpful websites
 var swipeXx = 0;
+swipeXxx
 var swipeInterval = true;
 function setSavedSwipe(){
 document.body.addEventListener('mousedown',function(event){ if(swipeInterval){
@@ -9,9 +10,9 @@ document.body.addEventListener('mouseup',function(event){ if(swipeInterval){
 document.body.addEventListener('keydown', function(event){if(swipeInterval){
         swipeInterval = false;if(event.keyCode == 39){nextSaved();} if(event.keyCode == 37){backSaved();}} setTimeout(function(){swipeInterval = true;},1000)});
 document.body.addEventListener('touchstart',function(event){ if(swipeInterval){
-        swipeInterval = false;swipeXx = event.touches[0].clientX} setTimeout(function(){swipeInterval = true;}, 1000)});
+        swipeInterval = false;swipeXxx = event.touches[0].clientX} setTimeout(function(){swipeInterval = true;}, 1000)});
 document.body.addEventListener('touchend',function(event){ if(swipeInterval){
-        swipeInterval = false;var swipeX2 = event.changedTouches[0].clientX; var swd = swipeX2-swipeXx; if(Math.abs(swd)>5){ if(swd<0){nextSaved();}else{backSaved();}}} setTimeout(function(){swipeInterval = true;},1000)});
+        swipeInterval = false;var swipeX2 = event.changedTouches[0].clientX; var swd = swipeX2-swipeXxx; if(Math.abs(swd)>5){ if(swd<0){nextSaved();}else{backSaved();}}} setTimeout(function(){swipeInterval = true;},1000)});
 
 document.body.addEventListener("wheel", function(event) {
     if(swipeInterval){
@@ -31,9 +32,9 @@ setTimeout(function(){swipeInterval = true;}, 1000)
 
 function endSavedSwipe(){
     document.body.removeEventListener('touchstart',function(event){ if(swipeInterval){
-        swipeInterval = false;swipeXx = event.touches[0].clientX} setTimeout(function(){swipeInterval = true;}, 1000)});
+        swipeInterval = false;swipeXxx = event.touches[0].clientX} setTimeout(function(){swipeInterval = true;}, 1000)});
 document.body.removeEventListener('touchend',function(event){ if(swipeInterval){
-        swipeInterval = false;var swipeX2 = event.changedTouches[0].clientX; var swd = swipeX2-swipeXx; if(Math.abs(swd)>5){ if(swd<0){nextSaved();}else{backSaved();}}} setTimeout(function(){swipeInterval = true;},1000)});
+        swipeInterval = false;var swipeX2 = event.changedTouches[0].clientX; var swd = swipeX2-swipeXxx; if(Math.abs(swd)>5){ if(swd<0){nextSaved();}else{backSaved();}}} setTimeout(function(){swipeInterval = true;},1000)});
 
 document.body.removeEventListener('mousedown',function(event){ if(swipeInterval){
         swipeInterval = false;swipeXx = event.clientX} setTimeout(function(){swipeInterval = true;}, 1000)});
