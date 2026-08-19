8df60db5314624fb20348dd4158a0f2f61c4b697
Ddyson "peiLamed" B <fmdasani@gmail.com>
2026-08-19 08:57:33 -0700

    Update script.js

--- script.js
+++ script.js
@@ -33,12 +33,12 @@ setTimeout(function(){swipeInterval = true;}, 1000)
 function endSavedSwipe(){
     document.body.removeEventListener('touchstart',function(event){ if(swipeInterval){
         swipeInterval = false;swipeXxx = event.touches[0].clientX} setTimeout(function(){swipeInterval = true;}, 1000)});
-document.body.addEventListener('touchend',function(event){ 
+document.body.removeEventListener('touchend',function(event){ 
         swipeInterval = false;var swipeX2 = event.changedTouches[0].clientX; var swd = swipeX2-swipeXxx; if(Math.abs(swd)>5){ if(swd<0){nextSaved();}else{backSaved();}}});
 
 document.body.removeEventListener('mousedown',function(event){ if(swipeInterval){
         swipeInterval = false;swipeXx = event.clientX} setTimeout(function(){swipeInterval = true;}, 1000)});
-document.body.addEventListener('mouseup',function(event){
+document.body.removeEventListener('mouseup',function(event){
         var swipeX2 = event.clientX; var swd = swipeX2-swipeXx; if(Math.abs(swd)>5){ if(swd<0){nextSaved();}else{backSaved();}}});
 document.body.removeEventListener('keydown',function(event){if(swipeInterval){
         swipeInterval = false;if(event.keyCode == 39){nextSaved();} if(event.keyCode == 37){backSaved();}} setTimeout(function(){swipeInterval = true;},1000)});
